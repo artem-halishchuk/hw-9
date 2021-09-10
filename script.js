141ade5e108c3cd91dc55e845d61179037fc97b1
Artem Halishchuk <artem.galishchuk@gmail.com>
2021-09-10 17:27:34 +0300

    added clearInterval

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ document.addEventListener("DOMContentLoaded", function() {
     content.style.display = "none";
     let preloaderItem = document.querySelector(".preloaderItem");
     let deg = 0;
-    setInterval(function () {
+    let t = setInterval(function () {
         deg += 8;
         preloaderItem.style.transform = 'rotate(' + deg + 'deg)';
         let rgb = () => Math.round(Math.random()*255);
         preloaderItem.style.backgroundColor = 'rgb(' + rgb() +','+ rgb() +','+ rgb() +')';
     }, 25)
-
+    window.onload = function () {
+        clearInterval(t);
+        let preloader = document.querySelector(".preloader");
+        preloader.style.display = "none";
+        let content = document.querySelector(".content");
+        content.style.display = "block";
+    }
 })
-window.onload = function () {
-    let preloader = document.querySelector(".preloader");
-    preloader.style.display = "none";
-    let content = document.querySelector(".content");
-    content.style.display = "block";
-}
